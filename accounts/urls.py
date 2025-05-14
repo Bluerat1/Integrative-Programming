5f952f7fbc28b3437ff43828360b51f24c9162c8
Bluerat1 <156603938+Bluerat1@users.noreply.github.com>
2025-05-15 03:16:12 +0800

    oh take me back to the start

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include ('djoser.urls')),
-    path('api/', include('djoser.urls.authtoken')),
+    path('bisag-unsa/', include ('djoser.urls')),
+    path('bisag-unsa/', include('djoser.urls.authtoken')),
 ]
